8990eed0b581a97aa587483ec0c9f95cec624cb2
syuuma-ito <itosyuuma@gmail.com>
2025-09-05 22:14:45 +0900

    [fix] Supabaseが複数ある問題を修正

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,7 +1,3 @@
-// lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "./supabase-client";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient();
