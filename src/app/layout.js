642c4f374d0fefc1045775aa27a5d0551b7e35fa
syuuma-ito <itosyuuma@gmail.com>
2025-09-06 06:04:25 +0900

    [add] ogp、ファビコンを設定

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,31 @@ const zenKakuGothic = Zen_Kaku_Gothic_New({
 export const metadata = {
     title: "みえっぷ 管理ページ | “三重がみえる” デジタル観光マップ",
     description: "みえっぷ 管理ページ | 三重大生が作った!! “三重がみえる” デジタル観光マップ",
+    metadataBase: new URL(process.env.NEXT_PUBLIC_URL),
+    openGraph: {
+        title: "みえっぷ 管理ページ | “三重がみえる” デジタル観光マップ",
+        description: "みえっぷ 管理ページ | 三重大生が作った!! “三重がみえる” デジタル観光マップ",
+        images: "/ogp.png",
+        type: "website",
+        locale: "ja",
+        site_name: "みえっぷ 管理ページ | “三重がみえる” デジタル観光マップ",
+    },
+    robots: {
+        index: false,
+        follow: false,
+        nocache: false,
+    },
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="ja">
+            <head>
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            </head>
             <body className={`${zenKakuGothic.variable} antialiased ${style.body}`}>
                 <AuthProvider>
                     <Header className={style.header} />
